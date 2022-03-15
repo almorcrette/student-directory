434fb2bfe284bbadd7bf63675722fbec89d8e885
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-03-15 08:00:33 +0000

    Completed exercise 7, adding functionality to input_students

--- directory.rb
+++ directory.rb
@@ -1,25 +1,69 @@
+# Input student
+
 def input_students
-  puts "Please enter the names of the studnts"
-  puts "To finish, just hit return twice"
-  # create an empty array
+  # Initialize students array
   students = []
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the stedent hash to the array
+  
+  # Receive preference to enter a new student or not
+  puts "Enter new student? 1 for yes, 0 for no"
+  new_entry = gets.chomp
+  
+  # Loop process for entering new students until user states no more
+  while new_entry != "0" do
+    
+    # Receive surname
+    puts "Enter surname:"
+    surname = gets.chomp
+    
+    # Receive first name
+    puts "Enter First name:"
+    first_name = gets.chomp
+    
+    # Start again if either field is empty
+    if surname == "" || first_name == ""
+      puts "Invalid entry. Fields must be non-empty. Try again"
+      next
+    end
+    
+    # Receive cohort, via loop until valid entry for cohort given
+    cohort = "not given"
+    until cohort != "not given"
+      puts "Enter cohort: (november, december, january, february)"
+      cohort = gets.chomp
+      cohort = "not given" if cohort != "november" && cohort != "december" && cohort != "january" && cohort != "february"
+      puts "Invalid entry for cohort. Try again."
+    end
+    cohort = cohort.to_sym
+    
+    # Option to re-enter for typos
+    puts "Sure you want to enter: #{first_name} #{surname}, cohort #{cohort}? 1 for yes. 0 for re-enter. x for finish"
+    selection = gets.chomp
+    case selection
+    when "0"
+      next
+    when "x"
+      break
+    end
+      
+    # Add entry to array
     students << {
-      name: name,
-      cohort: :november,
-      hobbies: "none given",
-      country_of_birth: "not given",
-      height: "not given"
-    }
+        name: "#{first_name} #{surname}",
+        cohort: cohort,
+        hobbies: "none given",
+        country_of_birth: "not given",
+        height: "not given"
+      }
+    
     puts "Now we have #{students.count} students"
-    # get another name from user#
-    name = gets.chomp
+    
+    # Receive preference for another entry
+    puts "Another entry? 1 for yes, 0 for no"
+    new_entry = gets.chomp
+  
   end# return the array of students
+  
   students
+  
 end
   
 def print_header
