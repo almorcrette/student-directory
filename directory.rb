e59f1fb901e242972df662630e94e2e9b9ee0c96
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-03-16 12:17:10 +0000

    refactor interactive menu

--- directory.rb
+++ directory.rb
@@ -1,56 +1,15 @@
-## Methods
-## This program has the following methods
-## - Start message
-## - Interactive menu
-## - Input students
-## - Print header
-## - Print footer
-## - Print sections of students by starting name letter
-## - Print students by cohorts
+# Initiate instance variable students, the array that will hold the directory
+@students = []
 
 # Start message
 def start_message
   puts "\n" + "Welcome to the Villains Academy Student Directory".center(100)
 end
 
-# Interactive menu
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-      when "1"
-        students = input_students
-      when "2" # show the students
-        if students.count == 0
-          puts "No students in directory"
-        else
-          print_header
-          print_cohorts(students)
-          print_footer(students)
-        end
-      when "9"
-        exit # this will cause the program to terminate
-      else
-        puts "I don't know what you meant, try again"
-    end
-  # 4. repeat from step 1
-  end
-end
-
-# Input student
+## Input student method
 
 def input_students
   
-  # Initiate an empty array to receive new student information
-  students = []
-
   # Receive preference to enter a new student or not
   # While loop with break prevents invalid entry (not 1 or 0)
   while true do
@@ -108,7 +67,7 @@ def input_students
     end
       
     # Add entry to array
-    students << {
+    @students << {
         name: "#{first_name} #{surname}",
         cohort: cohort,
         hobbies: "none given",
@@ -116,7 +75,7 @@ def input_students
         height: "not given"
       }
     
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{@students.count} students"
     
     # Receive preference for another entry,
     #including while loop with break to repeat until valid entry (1, 0)
@@ -128,9 +87,10 @@ def input_students
   
   end # return the array of students
   
-  students
-  
 end
+
+
+## Print methods
 
 # Header printer
 
@@ -141,8 +101,8 @@ end
 
 # Print students starting with a particular letter
 
-def print_section(students, letter)
-  students.each_with_index do |student, index|
+def print_section(letter)
+  @students.each_with_index do |student, index|
     if student[:name].chr.downcase == letter.downcase and student[:name].length < 12
       puts "#{index + 1}. #{student[:name]}, (#{student[:cohort]} cohort)
 Hobbies: #{student[:hobbies]},
@@ -152,31 +112,69 @@ Height: #{student[:height]}."
   end
 end
 
-# Print footer
-
-def print_footer(students)
-  puts students.count == 1 ? 
-  "Now we have 1 student".center(100) :
-  "Overall, we have #{students.count} great students".center(100)
-end
-
 # Print students by cohorts
 
-def print_cohorts(students)
-  cohorts = students.map { |student| student[:cohort] }.uniq
+def print_student_list_by_cohorts
+  cohorts = @students.map { |student| student[:cohort] }.uniq
   cohorts.each do |cohort|
     puts "#{cohort.capitalize}:"
-    students.each { |student| puts student[:name] if student[:cohort] == cohort }
+    @students.each { |student| puts student[:name] if student[:cohort] == cohort }
   end
 end
 
-puts start_message
-interactive_menu
-if students.count == 0
-  puts "No students in directory"
-else
-  print_header
-#print_section(students, "A")
-  print_cohorts(students)
-  print_footer(students)
+# Print footer
+
+def print_footer
+  puts @students.count == 1 ? 
+  "Now we have 1 student".center(100) :
+  "Overall, we have #{@students.count} great students".center(100)
 end
+
+## Interactive menu methods
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  if @students.count == 0
+    puts "No students in directory"
+  else
+    print_header
+    print_student_list_by_cohorts
+    print_footer
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2" # show the students
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+## Interactive menu
+# 1. print the menu and ask the user what to do
+# 2. read the input and save it into a variable
+# 3. do what the user has asked
+# 4. repeat from step 1
+
+def interactive_menu
+  loop do
+    print_menu 
+    selection = gets.chomp  
+    process(selection)
+  end
+end
+
+## Execute program
+
+interactive_menu
