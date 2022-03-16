1efe860a04354e026380be00636fe52e936bd031
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-03-16 12:57:37 +0000

    Save to file

--- directory.rb
+++ directory.rb
@@ -135,6 +135,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -154,6 +155,8 @@ def process(selection)
     input_students
   when "2" # show the students
     show_students
+  when "3"
+    save_students
   when "9"
     exit # this will cause the program to terminate
   else
@@ -174,6 +177,23 @@ def interactive_menu
     process(selection)
   end
 end
+
+## Saving directory to file
+# open the file for writing
+# iterate over the array of students
+
+def save_students
+  puts "Saving directory..."
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+  puts "Directory saved!"
+end
+  
 
 ## Execute program
 
